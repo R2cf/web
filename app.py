04b169ca41bc3b0771ff62012fac0bdbecad0451
Vijay sahu <88523094+R2cf@users.noreply.github.com>
2022-12-21 18:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://R2cf: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@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
